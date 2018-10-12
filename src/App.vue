1aa1a30fc7df575818f21d61e2b8549411fa215a
ShenHao <775308609@qq.com>
2018-10-12 09:29:21 +0800

    'cli'

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="header">
+      <v-header :seller='seller'></v-header>
+    </div>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to='/goods'>商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/ratings'>评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/seller'>商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+<script>
+
+import header from './components/header/header'
+export default {
+  name: 'App',
+  components:{
+    'v-header':header
+  },
+  data(){
+    return{
+      seller:{}
+    }
+  },
+  created(){
+    this.$http.get('/api/seller').then((res)=>{
+      res=JSON.parse(res.request.response)
+      if(res.code==0){
+        console.log(res)
+       this.seller=res.data
+      }
+    })
+  }
+}
+</script>
+
+<style lang="stylus">
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height :40px
+      .tab-item
+        flex:1
+        text-align:center
+        &>a
+          display:block
+          font-size:14px
+          color: rgb(77,85,93)
+          &.active
+            color:rgb(240,20,20)
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App';
+import axios from 'axios';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
+
+/* 安装vue-router插件 */
+Vue.use(VueRouter); // 注册路由
+Vue.prototype.$http = axios
+/* 定义路由页面 */
+const routes = [
+  // 这里 path: '/' 代表应用首页显示的内容
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+
+const router = new VueRouter({
+  // mode指定路由模式，默认'hash'，另一种可选的模式是'history'
+  mode: 'history',
+  linkActiveClass:'active',
+  routes
+});
+
+/* 创建挂载点至#app元素，将内容显示在index.html页面 */
+let app = new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
+
+Vue.use({
+  app
+});
